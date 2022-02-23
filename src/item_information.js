d58c3ba5a7197fb5b8850d547e5edccf9b697f79
kuro11pow2 <kuro11pow2@gmail.com>
2022-02-24 02:11:52 +0900

    설명 추가

--- src/item_information.js
+++ src/item_information.js
@@ -13,33 +13,34 @@ window.addEventListener("load", () => {
             row.appendChild(cell);
         }
         return row;
-    }    
-    
+    }
+
     tableHead.appendChild(CreateRow("th", [
-        "이름", 
-        "생산 단계", 
-        "소요 시간 총합 (초/개당)", 
-        "소요 코인 총합 (코인/개당)", 
-        "납품 점수 (점/개당)", 
+        "이름",
+        "생산 단계",
         "최대 생산량 (시간당)",
         "최대 소모량 (시간당)",
+        "소요 시간 총합 (초/개당)",
+        "소요 코인 총합 (코인/개당)",
+        "납품 점수 (점/개당)",
         "직접 요구 재료 (개당)",]));
     for (let item of Object.values(ItemFactory)) {
         if (item === ItemFactory.Crystal || item == ItemFactory.Coin) {
             continue;
         }
+        let Round = (pos, num) => Math.round(num * Math.pow(10, pos)) / Math.pow(10, pos);
         let inst = new item(0);
-        let mateiralsStr = inst.materials.map((material) => `${material.krName} ${Math.round(material.count/inst.outCount*100)/100}개`);
+        let mateiralsStr = inst.materials.map((material) => `${material.krName} ${Round(2, material.count / inst.outCount)}개`);
         let str = new Array(
-            inst.krName, 
-            inst.order, 
-            Math.round(inst.totalTimePerRequired*100)/100, 
-            Math.round(inst.totalCoinPerRequired*100)/100, 
-            inst.deliveryScore, 
-            Math.round(inst.outCount*3600/inst.timeRequired*100)/100,
-            Math.round(inst.maximumDemandPerHour*100)/100,
-            mateiralsStr,);
+            inst.krName,
+            inst.order,
+            Round(2, inst.outCount * 3600 / inst.timeRequired),
+            Round(2, inst.maximumDemandPerHour),
+            Round(2, inst.totalTimePerRequired),
+            Round(2, inst.totalCoinPerRequired),
+            inst.deliveryScore,
+            mateiralsStr);
         tableBody.appendChild(CreateRow("td", str))
     }
-    
+
 });
